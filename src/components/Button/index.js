86b9fd44eeba44abafeb59b6b0ec8188af67d75a
jayro <jayro1996.ag@gmail.com>
2021-10-05 11:56:32 -0500

    botones corregidos

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,70 +1,70 @@
 // import excel from '../../assets/images/excel.svg'
 export const ButtonOffice = ({ office /* = excel */ }) => {
   return (
-    <div className='flex items-center font-semibold text-gray-200 gap-x-2 w-52'>
+    <button className='flex items-center font-semibold text-gray-200 gap-x-2 w-52'>
       <img src={office} alt="" />
       <p>Descargar Historial</p>
-    </div>
+    </button>
   )
 }
 
-export const ButtonDownload = ({ text = 'Descargar', width = '', color = '', border = '', outline = false }) => {
+export const ButtonDownload = ({ text = 'Descargar', width = 'small', color = 'primary', border = '2', outline = false }) => {
   if (width === 'small') {
     if (outline === true) {
       return (
-        <div className={` text-${color}-500  px-6 py-2 bg-transparent  rounded-md text-center border-${border} border-${color}-500 font-semibold text-base `}>
+        <button className={` text-${color}-500  px-6 py-2 bg-transparent  rounded-md text-center border-${border} border-${color}-500 font-semibold text-base `}>
           {text}
-        </div>
+        </button>
       )
     } else {
       return (
-        <div className={`bg-${color}-500 px-6 py-2 text-white rounded-md text-center border-${border} border-${color}-500  font-medium text-base `}>
+        <button className={`bg-${color}-500 px-6 py-2 text-white rounded-md text-center border-${border} border-${color}-500  font-medium text-base `}>
           {text}
-        </div>
+        </button>
       )
     }
   } else if (width === 'medium') {
     if (border === !'') {
       if (outline === true) {
         return (
-          <div className={` text-${color}  px-9 py-1 tracking-widest bg-transparent rounded-md text-center border border-${color} font-semibold text-md `}>
+          <button className={` text-${color}  px-9 py-1 tracking-widest bg-transparent rounded-md text-center border border-${color} font-semibold text-md `}>
             {text}
-          </div>
+          </button>
         )
       } else {
         return (
-          <div className={`bg-${color} px-9 py-1 tracking-widest text-white  rounded-md text-center border border-${color}  font-semibold text-md `}>
+          <button className={`bg-${color} px-9 py-1 tracking-widest text-white  rounded-md text-center border border-${color}  font-semibold text-md `}>
             {text}
-          </div>
+          </button>
         )
       }
     } else {
       if (outline === true) {
         return (
-          <div className={` text-${color}  px-9 py-1 tracking-widest bg-transparent rounded-md text-center border${border} border-${color} font-semibold text-md `}>
+          <button className={` text-${color}  px-9 py-1 tracking-widest bg-transparent rounded-md text-center border${border} border-${color} font-semibold text-md `}>
             {text}
-          </div>
+          </button>
         )
       } else {
         return (
-          <div className={`bg-${color} px-9 py-1 tracking-widest text-white  rounded-md text-center border${border} border-${color}  font-semibold text-md `}>
+          <button className={`bg-${color} px-9 py-1 tracking-widest text-white  rounded-md text-center border${border} border-${color}  font-semibold text-md `}>
             {text}
-          </div>
+          </button>
         )
       }
     }
   } else if (width === 'large') {
     if (outline === true) {
       return (
-        <div className={` text-${color}  px-8 py-2 tracking-wide bg-transparent   rounded-md text-center border-${border} border-${color} font-medium text-base `}>
+        <button className={` text-${color}  px-8 py-2 tracking-wide bg-transparent   rounded-md text-center border-${border} border-${color} font-medium text-base `}>
           {text}
-        </div>
+        </button>
       )
     } else {
       return (
-        <div className={`bg-${color} px-8 py-2 tracking-wide text-white border-${border} border-${color} rounded-md text-center  font-medium text-base `}>
+        <button className={`bg-${color} px-8 py-2 tracking-wide text-white border-${border} border-${color} rounded-md text-center  font-medium text-base `}>
           {text}
-        </div>
+        </button>
       )
     }
   }
