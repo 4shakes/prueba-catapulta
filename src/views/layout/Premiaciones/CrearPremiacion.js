a8beacbcbe3ef6b5a1e3b9c955aba1c72bab4862
jayro <jayro1996.ag@gmail.com>
2021-10-12 10:34:24 -0500

    vista de crear premiacion terminada

--- src/views/layout/Premiaciones/CrearPremiacion.js
+++ src/views/layout/Premiaciones/CrearPremiacion.js
@@ -4,10 +4,17 @@ import { ButtonDownload } from '../../../components/Button'
 import InputUploadImage from '../../../components/inputs/InputUploadImage'
 
 const CrearPremiacion = () => {
+  /* RUTA QUE PUSE DE MODELO Y QUE NO FUNCIONA EN MI PROYECTO ES PARA QUE LO IMPLEMENTEN EN EL PROYECTO PRINCIPAL */
+  const crumbs = [
+    { path: '/gestion-premiacion', label: 'Gestión de Premiaciones' },
+    { path: '/gestion-premiacion/partes-de-la-premiacion]', label: 'Partes de la Premiación' }
+  ]
+  /* RUTA QUE PUSE DE MODELO Y QUE NO FUNCIONA EN MI PROYECTO ES PARA QUE LO IMPLEMENTEN EN EL PROYECTO PRINCIPAL */
+
   return (
     <div className="">
-      <Breadcrumb />
-      <div className="w-full pb-8 md:max-w-280 mx-auto flex justify-center lg:justify-center px-3">
+      <Breadcrumb crumbs={crumbs} />
+      <div className="w-full pb-8 mt-4 md:mt-2 md:max-w-280 mx-auto flex justify-center lg:justify-center px-3">
         <div className="flex md:justify-end md:mt-10 md:mb-8 md:gap-x-10 " >
           <div className="mx-7  md:pl-15 lg:w-200" >
             <h2 className="text-2xl text-primary text-center font-bold mb-6">Completa los datos de cada parte de la premiación</h2>
@@ -18,7 +25,7 @@ const CrearPremiacion = () => {
             <InputUploadImage button={true} icon={false} text="Parte 5" placeholder="Nombre de la Parte 5" textButton="Editar" />
             <InputUploadImage button={true} icon={false} text="Parte 6" placeholder="Nombre de la Parte 6" textButton="Editar" />
             <div className="w-64 mx-auto mt-7">
-              <ButtonDownload width="large" text="CREAR PREMIACION" color='primary' outline={true} border='2' />
+              <ButtonDownload width="small" text="CREAR PREMIACION" color='primary' outline={true} border='2' />
             </div>
           </div>
         </div>
